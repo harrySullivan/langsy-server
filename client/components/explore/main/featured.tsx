147d8612487b3bc0e8026751eea6c488c9ad96e6
Harry Sullivan <aditya.michigan@gmail.com>
2019-10-14 20:52:48 -0400

    featured

--- client/components/explore/main/featured.tsx
+++ client/components/explore/main/featured.tsx
@@ -1,9 +1,27 @@
 import { h } from 'preact'
+import "./featured.css"
+import { SectionHeading } from './section-heading'
 
-export const Main = (
+const Item = (
+	(primaryInfo: string, secondaryInfo: string, thumbnail: string) => (
+		<div class="column is-4">
+			<img class="featured-thumbnail" src={thumbnail} />
+			<h5 class="has-text-weight-bold ml1 mt1">{primaryInfo}</h5>
+			<h6 class="has-text-grey-light ml1">{secondaryInfo}</h6>
+		</div>
+	)
+)
+
+export const Featured = (
   () => (
   	<div class="column">
-	    Main
+	    {SectionHeading("featured", "albums")}
+	    <div class="columns pl2 results-wrapper">
+	    	{Item("Blue Moon", "Hayke", "https://www.theaudiodb.com/images/media/artist/thumb/uxrqxy1347913147.jpg")}
+	    	{Item("Blue Moon", "Hayke", "https://www.theaudiodb.com/images/media/artist/thumb/uxrqxy1347913147.jpg")}
+	    	{Item("Blue Moon", "Hayke", "https://www.theaudiodb.com/images/media/artist/thumb/uxrqxy1347913147.jpg")}
+	    	{Item("Blue Moon", "Hayke", "https://www.theaudiodb.com/images/media/artist/thumb/uxrqxy1347913147.jpg")}
+	    </div>
     </div>
   )
 )
