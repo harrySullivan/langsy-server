71bc1a52205fc9c7518d676f8c1fa356f1f57b3b
Harry Sullivan <aditya.michigan@gmail.com>
2019-11-10 10:29:05 -0500

    remove comments

--- http/controllers/DefaultController.go
+++ http/controllers/DefaultController.go
@@ -8,16 +8,13 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
-// Controller is a generic struct
 type Controller struct {
 }
 
-// Collection returns the collection from the database
 func (Controller) Collection(collectionName string) *mongo.Collection {
 	return database.Collection(collectionName)
 }
 
-// Log is a logger
 func (Controller) Log(c *gin.Context) *log.Entry {
 	contextualizedLogger := log.WithFields(
 		log.Fields{
--- http/controllers/MainController.go
+++ http/controllers/MainController.go
@@ -4,19 +4,16 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// MainController is the Main handler for the client and pinger
 type MainController struct {
 	Controller
 }
 
-// Homepage is just a page to make fun of fools
 func (MainController) Homepage(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"message": "Langsy Server. Leave or DIE.",
 	})
 }
 
-// Ping is used for uptime
 func (MainController) Ping(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"message": "ashwat watches fifty shades of grey",
